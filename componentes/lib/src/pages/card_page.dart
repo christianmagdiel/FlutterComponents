15abd57912bdd45e32471121b80b030a363f2a15
cristian alvarado esperano <Cris_ales@LIVE.COM.MX>
2019-10-24 17:53:41 -0600

    Se creo un Card para mostrar una imagen

--- componentes/lib/src/pages/card_page.dart
+++ componentes/lib/src/pages/card_page.dart
@@ -8,9 +8,11 @@ class CardPage extends StatelessWidget {
         title: Text('Cards'),
       ),
       body: ListView(
-        padding: EdgeInsets.all(10),
+        padding: EdgeInsets.all(10.0),
         children: <Widget>[
-          _cardTipo1()   
+          _cardTipo1(),
+          SizedBox(height: 30.0,),
+          _cardTipo2()   
         ],
       ),
     );
@@ -38,6 +40,29 @@ class CardPage extends StatelessWidget {
               )
             ],
           )
+        ],
+      ),
+    );
+  }
+
+  Widget _cardTipo2() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          FadeInImage(
+            image: NetworkImage('https://www.tom-archer.com/wp-content/uploads/2018/06/milford-sound-night-fine-art-photography-new-zealand.jpg'), 
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            fadeInDuration: Duration(milliseconds: 200),
+            height: 300.0,
+            fit: BoxFit.cover,
+          ),
+          // Image(
+          //   image: NetworkImage('https://www.tom-archer.com/wp-content/uploads/2018/06/milford-sound-night-fine-art-photography-new-zealand.jpg'), 
+          // ),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('Imagen de muestra')
+          ),
         ],
       ),
     );
