6a814169ce6dec156ba7c927ed6c754e1597698a
cristian alvarado esperano <Cris_ales@LIVE.COM.MX>
2019-10-25 17:40:35 -0600

    Pantalla Input, configuracion para poner lenguale en español a fechas.

--- componentes/lib/main.dart
+++ componentes/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:componentes/src/routes/routes.dart';
 import 'package:componentes/src/pages/alert_page.dart';
- 
+import 'package:flutter_localizations/flutter_localizations.dart';
+
 void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
@@ -10,6 +11,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Componentes App',
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+      // ... app-specific localization delegate[s] here
+      GlobalMaterialLocalizations.delegate,
+      GlobalWidgetsLocalizations.delegate,
+      GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+      const Locale('en','US'), // English
+      const Locale('es','ES'), // Español
+      // ... other locales the app supports
+      ],
       //home: HomePage(),
       initialRoute: '/',
       routes: getApplicationRoutes(),
